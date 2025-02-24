a9813061564911e3e1f43f1c9b92ae2e6052c96c
grelkalrz <182034229+grelylrz@users.noreply.github.com>
2025-02-24 06:18:46 +0500

    Packet send

--- src/example/BVars.java
+++ src/example/BVars.java
@@ -3,13 +3,15 @@ package example;
 import mindustry.core.NetClient;
 import mindustry.core.Platform;
 import mindustry.net.Net;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.nio.channels.SocketChannel;
 
 public class BVars {
     public static int pport = 6571;
     public static String ip = "121.127.37.17";
-    // region packet send
-    public static Platform platform = new Platform() {};
-    public static Net.NetProvider ale = platform.getNet();
-    public static Net net2 = new Net(ale);
-    // public static NetClient client = new NetClient();
+
 }
--- src/example/Main.java
+++ src/example/Main.java
@@ -1,57 +1,38 @@
 package example;
 
-import arc.Core;
-import arc.func.Cons;
 import arc.math.Rand;
 import arc.util.Log;
 import arc.struct.*;
-import arc.util.Time;
-import arc.util.io.ReusableByteInStream;
 import arc.util.serialization.Base64Coder;
 import mindustry.Vars;
-import mindustry.content.UnitTypes;
-import mindustry.core.GameState;
-import mindustry.core.Logic;
-import mindustry.core.Control;
-import mindustry.entities.EntityGroup;
-import mindustry.gen.*;
+import mindustry.core.Platform;
+import mindustry.gen.ConnectConfirmCallPacket;
 import mindustry.net.*;
-import static mindustry.Vars.*;
-import mindustry.core.NetClient;
-import mindustry.logic.*;
-import mindustry.type.*;
-
-import java.io.DataInputStream;
-import java.util.Locale;
+import mindustry.net.Packets.*;
 import java.util.zip.InflaterInputStream;
 
+import java.net.*;
+import java.util.Locale;
+
+import static mindustry.Vars.*;
 import static example.BVars.*;
 
+/*
+https://github.com/Kieaer/MindustryBotnet/tree/main/src/main
+https://github.com/Anuken/Mindustry/blob/master/core/src/mindustry/net/ArcNetProvider.java
+https://github.com/Anuken/Mindustry/blob/master/core/src/mindustry/net/Net.java
+https://github.com/Anuken/Mindustry/blob/master/core/src/mindustry/core/NetClient.java
+ */
+
 public class Main {
-
-    /** List of entities that were removed, and need not be added while syncing. */
-    private IntSet removed = new IntSet();
-    /** Byte stream for reading in snapshots. */
-    private ReusableByteInStream byteStream = new ReusableByteInStream();
-    private DataInputStream dataStream = new DataInputStream(byteStream);
-    /** Packet handlers for custom types of messages. */
-    private ObjectMap<String, Seq<Cons<String>>> customPacketHandlers = new ObjectMap<>();
-    /** Packet handlers for custom types of messages, in binary. */
-    private ObjectMap<String, Seq<Cons<byte[]>>> customBinaryPacketHandlers = new ObjectMap<>();
-
+    static Platform platform = new Platform() {};
+    static Net.NetProvider ale = platform.getNet();
+    static Net net2 = new Net(ale);
     public static void main(String[] args) {
         Vars.loadLogger();
         net = net2;
-        NetClient client2 = new NetClient();
-        netClient = client2;
         Log.info("Inited");
-        /*Vars.logic = new Logic();
-        state = new GameState();
-        // groups init
-        control = new Control();
-        // костыль
-        MapPreviewLoader.setupLoaders();*/
-        if(args != null && args.length > 0) {
+        if(args != null) {
             for (String arg : args) {
                 Log.info(arg);
             }
@@ -60,73 +41,66 @@ public class Main {
         Log.info("generating packet");
         String test = randomString();
         Log.info("Your usid/uuid gen result " + test + " " + test.length());
-        if(test.length() > 12 || test.length() < 12) {
+        if(test.length() != 12) {
             Log.err("Your usid/uuid gen is gen's >/< than 12 symb.");
         }
-        var c = new Packets.ConnectPacket();
-        c.name = "grely test bot";
-        c.locale = Locale.getDefault().toString();;
-        c.mods = new Seq<>();
-        c.mobile = false;
-        c.versionType = "official";
-        c.color = 749469439;
-        c.usid = randomString();
-        c.uuid = randomString();
-        // region send
+        String locale = Locale.getDefault().toString();
+        Log.info("Bot locale " + locale);
+        Log.info("Adding handlers...");
+        net2.handleClient(Connect.class, packet -> {
+            Log.info("Connecting to server: @", packet.addressTCP);
+            var c = new Packets.ConnectPacket();
+            c.name = "grely test bot";
+            c.locale = locale;
+            c.mods = new Seq<>();
+            c.mobile = false;
+            c.versionType = "official";
+            c.color = 749469439;
+            c.usid = randomString();
+            c.uuid = randomString();
+            net2.send(c, true);
+        });
+        net2.handleClient(Disconnect.class, packet -> {
+            if(packet.reason != null){
+                switch(packet.reason) {
+                    case "closed" -> Log.warn("disconnect.closed");
+                    case "timeout" -> Log.warn("disconnect.timeout");
+                    default -> Log.warn("disconnect.error");
+                }
+            } else{
+                Log.warn("Connect.closed");
+            }
+        });
+        net2.handleClient(WorldStream.class, data -> {
+            Log.info("Received world data: @ bytes.", data.stream.available());
+            NetworkIO.loadWorld(new InflaterInputStream(data.stream));
+
+            finishConnecting();
+        });
+        Log.info("Handlers added");
         try {
-            ale.connectClient(ip, pport, () -> {
-                send(c, true);
-                net2.handleClient(Packets.WorldStream.class, data -> {
-                    Log.info("Received world data: @ bytes.", data.stream.available());
-                    finishConnecting();
-                });
+            Log.info("Trying to connect...");
+            net2.connect(ip, pport, () -> {
+                Log.info("Connecting to " + ip + ":" + pport);
             });
-        } catch (Exception e) {Log.err("Error while ale.connectClient ", e);}
-    }
-
-    public static void send(Object object, boolean reliable){
-        Log.info("send used"); // DEBUG
-        ale.sendClient(object, reliable);
-    }
-
-    public static void confirm() {
-        Log.info("Confirming connect");
-        ConnectConfirmCallPacket packet = new ConnectConfirmCallPacket();
-        send(packet, true);
-    }
-
-    public static void disconnect(){
-        Log.info("Disconnecting.");
-        ale.disconnectClient();
+        } catch (Exception e) {
+            Log.err("Error!", e);
+        }
     }
 
     public static String randomString() {
         byte[] bytes = new byte[8];
         new Rand().nextBytes(bytes);
-        String result = new String(Base64Coder.encode(bytes));
-        return result;
+        return new String(Base64Coder.encode(bytes));
     }
 
-    private static void finishConnecting(){
-        state.set(GameState.State.playing);
-        net.setClientLoaded(true);
-        confirm();
+    public static void connectConfirmm() {
+        ConnectConfirmCallPacket packet = new ConnectConfirmCallPacket();
+        net2.send(packet, true);
     }
 
-    // anuke
-    public void addPacketHandler(String type, Cons<String> handler){
-        customPacketHandlers.get(type, Seq::new).add(handler);
-    }
-
-    public Seq<Cons<String>> getPacketHandlers(String type){
-        return customPacketHandlers.get(type, Seq::new);
-    }
-
-    public void addBinaryPacketHandler(String type, Cons<byte[]> handler){
-        customBinaryPacketHandlers.get(type, Seq::new).add(handler);
-    }
-
-    public Seq<Cons<byte[]>> getBinaryPacketHandlers(String type){
-        return customBinaryPacketHandlers.get(type, Seq::new);
+    public static void finishConnecting(){
+        net2.setClientLoaded(true);
+        connectConfirmm();
     }
 }

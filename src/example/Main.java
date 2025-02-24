bfc74de640e9d994b18658bf997dd8aafbb551be
grelkalrz <182034229+grelylrz@users.noreply.github.com>
2025-02-24 07:09:23 +0500

    наработки.р

--- src/example/Main.java
+++ src/example/Main.java
@@ -1,18 +1,26 @@
 package example;
 
 import arc.math.Rand;
+import arc.net.Client;
 import arc.util.Log;
 import arc.struct.*;
+import arc.util.Threads;
 import arc.util.serialization.Base64Coder;
 import mindustry.Vars;
+import mindustry.core.NetClient;
 import mindustry.core.Platform;
 import mindustry.gen.ConnectConfirmCallPacket;
 import mindustry.net.*;
 import mindustry.net.Packets.*;
+
+import java.io.IOException;
 import java.util.zip.InflaterInputStream;
 
+import mindustry.net.ArcNetProvider.*;
 import java.net.*;
 import java.util.Locale;
+import arc.net.Client.*;
+import java.nio.channels.ClosedSelectorException;
 
 import static mindustry.Vars.*;
 import static example.BVars.*;
@@ -22,15 +30,19 @@ https://github.com/Kieaer/MindustryBotnet/tree/main/src/main
 https://github.com/Anuken/Mindustry/blob/master/core/src/mindustry/net/ArcNetProvider.java
 https://github.com/Anuken/Mindustry/blob/master/core/src/mindustry/net/Net.java
 https://github.com/Anuken/Mindustry/blob/master/core/src/mindustry/core/NetClient.java
+https://github.com/Anuken/Arc/blob/master/extensions/arcnet/src/arc/net/Client.java
  */
 
 public class Main {
     static Platform platform = new Platform() {};
     static Net.NetProvider ale = platform.getNet();
     static Net net2 = new Net(ale);
+    static ArcNetProvider p = new ArcNetProvider();
+    static Client client;
     public static void main(String[] args) {
         Vars.loadLogger();
         net = net2;
+        netClient = new NetClient();
         Log.info("Inited");
         if(args != null) {
             for (String arg : args) {
@@ -38,6 +50,7 @@ public class Main {
             }
         }
         // region packet
+        client = new Client(8192, 16384, new PacketSerializer());
         Log.info("generating packet");
         String test = randomString();
         Log.info("Your usid/uuid gen result " + test + " " + test.length());
@@ -80,7 +93,7 @@ public class Main {
         Log.info("Handlers added");
         try {
             Log.info("Trying to connect...");
-            net2.connect(ip, pport, () -> {
+            connectClient(ip, pport, () -> {
                 Log.info("Connecting to " + ip + ":" + pport);
             });
         } catch (Exception e) {
@@ -102,5 +115,29 @@ public class Main {
     public static void finishConnecting(){
         net2.setClientLoaded(true);
         connectConfirmm();
+    }
+
+    public static void connectClient(String ip, int port, Runnable success) {
+        /*Threads.start(() -> {*/
+            try {
+                Log.info("Connecting to " + ip + ":" + port);
+                client.connect(50000, ip, port, port);
+                Threads.daemon("Client Update", () -> {
+                    try {
+                        while (client.isConnected()) {
+                            client.update(500);
+                        }
+                    } catch (IOException e) {
+                        Log.err("Error during client update", e);
+                    }
+                });
+                Log.info("Connected to " + ip + ":" + port);
+                success.run();
+                Log.info("Your code runned after connection");
+
+            } catch (Exception e) {
+                Log.err("Error during connection", e);
+            }
+        /*});*/
     }
 }

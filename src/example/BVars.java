bbf8e758c449de60c50cf8939077ed3a25f3ae3c
grely <182034229+grelylrz@users.noreply.github.com>
2025-04-22 22:10:49 +0500

    Update BVars.java

--- src/example/BVars.java
+++ src/example/BVars.java
@@ -5,5 +5,5 @@ import arc.struct.Seq;
 public class BVars {
     public static int pport = 6571;
     public static String ip = "121.127.37.17";
-    public static Seq<String> bannedErrs = Seq.with("ui", "TextFormatter", "renderer", "reading entity", "enableEffects", "entity", "mindustry.gen.LogicExplosionCallPacket.handled(LogicExplosionCallPacket.java:54)", "EOFException");
+    public static Seq<String> bannedErrs = Seq.with("ui", "TextFormatter", "renderer", "reading entity", "enableEffects", "entity", "mindustry.gen.LogicExplosionCallPacket.handled(LogicExplosionCallPacket.java:54)", "EOFException", "null");
 }
